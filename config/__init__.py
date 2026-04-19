f0c5320e7e5f04a7bdc18107ca0be393abc5c49a
DatAlbertW <144963224+DatAlbertW@users.noreply.github.com>
2026-04-19 14:28:54 +0200

    Create __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+"""Editable configuration files for MedScope.
+
+Everything in this package is meant to be tuned by the user:
+molecule lists, categories, prompts, scoring weights, filters.
+
+Do NOT put engine logic here.
+"""
